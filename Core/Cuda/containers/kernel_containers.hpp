76c7f788a04bb0169338fe70d22d2ac1e402976e
Wan Youwen <1756429307@qq.com>
2018-11-12 21:06:12 +0800

    gpu数据 结构体

--- Core/Cuda/containers/kernel_containers.hpp
+++ Core/Cuda/containers/kernel_containers.hpp
@@ -1,35 +1,6 @@
 /*
  * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2011, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
+ * 
  *
  *  Author: Anatoly Baskeheev, Itseez Ltd, (myname.mysurname@mycompany.com)
  */
@@ -39,19 +10,20 @@
 
 #include <cstddef>
 
-#if defined(__CUDACC__)
+#if defined(__CUDACC__) // Defined when compiling CUDA source files. 
 #define GPU_HOST_DEVICE__ __host__ __device__ __forceinline__
 #else
 #define GPU_HOST_DEVICE__
 #endif
 
+// 设备 结构体数据
 template <typename T>
 struct DevPtr {
-  typedef T elem_type;
-  const static size_t elem_size = sizeof(elem_type);
+  typedef T elem_type;//数据 类型
+  const static size_t elem_size = sizeof(elem_type);//数据单位长度 字节
 
-  T* data;
-
+  T* data;// 数据起始地址
+// cpu数据??  主GPU数据？？
   GPU_HOST_DEVICE__ DevPtr() : data(0) {}
   GPU_HOST_DEVICE__ DevPtr(T* data_arg) : data(data_arg) {}
 
