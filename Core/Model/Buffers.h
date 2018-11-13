ef0f4258ad9c905d66c25fcc0ea755df252355ec
Wan Youwen <1756429307@qq.com>
2018-11-13 16:31:14 +0800

    GLuint 就是正整形，和C里面的unsigned int 一样  数据缓冲区&&目标状态

--- Core/Model/Buffers.h
+++ Core/Model/Buffers.h
@@ -1,6 +1,8 @@
 #pragma once
 
 struct OutputBuffer {
-  GLuint dataBuffer = 0;
-  GLuint stateObject = 0;
+// GLuint 就是正整形，和C里面的unsigned int 一样
+  // openGL里面定义的
+  GLuint dataBuffer = 0; // 数据缓冲区
+  GLuint stateObject = 0;// 目标状态
 };
