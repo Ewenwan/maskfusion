b0a07d2cb8e753b38ed17c8fc8c501b5c646211d
Wan Youwen <1756429307@qq.com>
2018-11-13 21:10:16 +0800

    Update GPUTexture.h

--- Core/GPUTexture.h
+++ Core/GPUTexture.h
@@ -1,18 +1,6 @@
 /*
  * This file is part of ElasticFusion.
  *
- * Copyright (C) 2015 Imperial College London
- *
- * The use of the code within this file and all code within files that
- * make up the software that is ElasticFusion is permitted for
- * non-commercial purposes only.  The full terms and conditions that
- * apply to the code within this file are detailed within the LICENSE.txt
- * file and at <http://www.imperial.ac.uk/dyson-robotics-lab/downloads/elastic-fusion/elastic-fusion-license/>
- * unless explicitly stated.  By downloading this file you agree to
- * comply with these terms.
- *
- * If you wish to use any of this code for commercial purposes then
- * please email researchcontracts.engineering@imperial.ac.uk.
  *
  */
 
@@ -28,8 +16,13 @@
 class GPUTexture {
  public:
 
-  GPUTexture(const int width, const int height, const GLint internalFormat, const GLenum format, const GLenum dataType,
-             const bool drawAndSampleLinear = false, const bool cuda = false, unsigned int cudaFlags = cudaGraphicsRegisterFlagsReadOnly);
+  GPUTexture(const int width, const int height,
+             const GLint internalFormat, 
+             const GLenum format, 
+             const GLenum dataType,
+             const bool drawAndSampleLinear = false, 
+             const bool cuda = false, 
+             unsigned int cudaFlags = cudaGraphicsRegisterFlagsReadOnly);
 
   virtual ~GPUTexture();
 
@@ -56,7 +49,9 @@ class GPUTexture {
   const GLenum dataType;
 
  private:
-  GPUTexture() : texture(nullptr), draw(false), width(0), height(0), internalFormat(0), format(0), dataType(0), cudaRes(nullptr), cudaSurfHandle(0) {}
+  GPUTexture() : texture(nullptr), draw(false),
+                 width(0), height(0), internalFormat(0), 
+                 format(0), dataType(0), cudaRes(nullptr), cudaSurfHandle(0) {}
 
   bool cudaIsMapped = false;
   cudaGraphicsResource* cudaRes;
