406bb68204cee4ed9e1340eae8ad0fe968822c87
Wan Youwen <1756429307@qq.com>
2018-11-13 22:12:37 +0800

    Update Segmentation.h

--- Core/Segmentation/Segmentation.h
+++ Core/Segmentation/Segmentation.h
@@ -1,18 +1,6 @@
 /*
  * This file is part of https://github.com/martinruenz/maskfusion
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
  */
 
 #pragma once
@@ -30,7 +18,8 @@
 class Model;
 class GPUTexture;
 
-class Segmentation {
+class Segmentation
+{
  public:
   enum class Method { CO_FUSION, MASK_FUSION, PRECOMPUTED };
 
@@ -46,7 +35,8 @@ class Segmentation {
 
   std::vector<std::pair<std::string, std::shared_ptr<GPUTexture> > > getDrawableTextures();
 
-  SegmentationResult performSegmentation(std::list<std::shared_ptr<Model>>& models, FrameDataPointer frame, unsigned char nextModelID,
+  SegmentationResult performSegmentation(std::list<std::shared_ptr<Model>>& models, 
+                                         FrameDataPointer frame, unsigned char nextModelID,
                                          bool allowNew);
 
 //  SegmentationResult performSegmentationPrecomputed(std::list<std::shared_ptr<Model>>& models, FrameDataPointer frame, unsigned char nextModelID,
