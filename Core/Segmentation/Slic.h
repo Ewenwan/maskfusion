08f256e7727be116a22acce08744a4821004462e
Wan Youwen <1756429307@qq.com>
2018-11-13 22:01:24 +0800

    SLIC 简单的线性迭代聚类   超像素分割算法

--- Core/Segmentation/Slic.h
+++ Core/Segmentation/Slic.h
@@ -1,18 +1,10 @@
 /*
  * This file is part of https://github.com/martinruenz/maskfusion
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * SLIC超像素分割算法  SLIC（simple linear iterativeclustering），即简单的线性迭代聚类 
+ 它是2010年提出的一种思想简单、实现方便的算法，将彩色图像转化为CIELAB颜色空间和XY坐标下的5维特征向量，
+ 然后对5维特征向量构造距离度量标准，对图像像素进行局部聚类的过程。
+ 
+ 
  */
 
 #pragma once
@@ -28,11 +20,15 @@
 class Slic {
  public:
   Slic() {}
-  Slic(unsigned width, unsigned height, int spixelSize, /*float scale,*/ gSLICr::COLOR_SPACE colorSpace = gSLICr::XYZ);
+  Slic(unsigned width, unsigned height, 
+       int spixelSize,
+       /*float scale,*/
+       gSLICr::COLOR_SPACE colorSpace = gSLICr::XYZ);
 
   virtual ~Slic() {}
 
   inline bool isValid() { return slicInput && engine; }
+ 
   inline unsigned getSuperpixelSize(unsigned index) const { return spixelCounts[index]; }
 
   // Returns CPU-Pointer of data (not expensive, no copying)
@@ -46,7 +42,8 @@ class Slic {
 
   /// create downsampling of other images (same scene as input image)
   template <typename T>
-  cv::Mat downsample(const cv::Mat& image, int channel = -1) const {
+  cv::Mat downsample(const cv::Mat& image, int channel = -1) const
+  {
     assert(image.isContinuous());
     assert(image.type() != CV_8UC1);  // Otherwise overflows are very likely
     assert(image.type() != cv::DataType<unsigned char>::type);
@@ -60,23 +57,29 @@ class Slic {
 
     const int* slic = getResult();
 
-    if (image.channels() == 1) {
+    if (image.channels() == 1) 
+    {
       for (unsigned index = 0; index < image.total(); index++) resData[slic[index]] += inData[index];
-    } else {
+    } 
+    else 
+    {
       assert(channel >= 0 && channel < image.channels());
       unsigned cIndex = channel;
       unsigned cStep = image.channels();
-      for (unsigned index = 0; index < image.total(); index++) {
+      for (unsigned index = 0; index < image.total(); index++)
+      {
         resData[slic[index]] += inData[cIndex];
         cIndex += cStep;
       }
     }
 
     // Resample empty super-pixel, divide result by size of super-pixel
-    for (unsigned index = 0; index < spixelNum; index++) {
+    for (unsigned index = 0; index < spixelNum; index++)
+    {
       int cnt = spixelCounts[index];
       int readIndex = index;
-      if (cnt == 0) {
+      if (cnt == 0) 
+      {
         readIndex = resampleEmptyIndex(index);
         cnt = spixelCounts[readIndex];
         assert(cnt > 0);
@@ -87,9 +90,11 @@ class Slic {
     return result;
   }
 
+ // 下采样==================================
   /// create downsampling of other images (same scene as input image)
   template <typename T>
-  cv::Mat downsampleThresholded(const cv::Mat& image, T minThreshold) const {
+  cv::Mat downsampleThresholded(const cv::Mat& image, T minThreshold) const 
+  {
     assert(image.isContinuous());
     assert(image.type() != CV_8UC1);  // Otherwise overflows are very likely
     assert(image.type() != cv::DataType<unsigned char>::type);
@@ -103,8 +108,10 @@ class Slic {
     const int* slic = getResult();
     std::vector<unsigned> downsampleCounts(result.total(), 0);
 
-    for (unsigned index = 0; index < image.total(); index++) {
-      if (inData[index] > minThreshold) {
+    for (unsigned index = 0; index < image.total(); index++) 
+    {
+      if (inData[index] > minThreshold)
+      {
         resData[slic[index]] += inData[index];
         downsampleCounts[slic[index]]++;
       }
@@ -130,8 +137,10 @@ class Slic {
     return upsample<T, T>(input);
   }
 
+ // 上采样==========================
   template <typename Tin, typename Tout>
-  cv::Mat upsample(const cv::Mat input) {
+  cv::Mat upsample(const cv::Mat input) 
+ {
     // assert(input.channels() == 1);
     assert(input.isContinuous());
     assert(input.total() == spixelNum);
@@ -147,17 +156,20 @@ class Slic {
   }
 
 #ifdef SHOW_DEBUG_VISUALISATION
-  inline void createSlicColors(unsigned num) {
+  inline void createSlicColors(unsigned num) 
+  {
     unsigned color = 0xa3b241;
     slicColors.reserve(num);
-    for (unsigned i = slicColors.size(); i < num; i++) {
+    for (unsigned i = slicColors.size(); i < num; i++) 
+    {
       color = color * (3 * i + i + 1);
       color = (color & (0xffffff)) >> 2;
       slicColors.push_back(cv::Vec3b((color & 0xff0000) >> 16, (color & 0x00ff00) >> 8, color & 0x0000ff));
     }
   }
 
-  inline cv::Mat drawSurfelColors() {
+  inline cv::Mat drawSurfelColors() 
+  {
     createSlicColors(spixelNum);
     cv::Mat res(slicResult->noDims.y, slicResult->noDims.x, CV_8UC3);
     const int* data = slicResult->GetData(MEMORYDEVICE_CPU);
@@ -168,7 +180,8 @@ class Slic {
 
   inline cv::Mat drawSurfelBorders(bool swapRedBlue) { return drawSurfelBorders(inputImage, swapRedBlue); }
 
-  inline cv::Mat drawSurfelBorders(cv::Mat image, bool swapRedBlue) {
+  inline cv::Mat drawSurfelBorders(cv::Mat image, bool swapRedBlue) 
+  {
     cv::Mat res;
     if (image.type() == CV_8UC3)
       image.copyTo(res);
@@ -178,8 +191,10 @@ class Slic {
       assert(0);
     if (swapRedBlue) cv::cvtColor(res, res, cv::COLOR_RGB2BGR);
     const int* slic = slicResult->GetData(MEMORYDEVICE_CPU);
-    for (int y = 1; y < slicResult->noDims.y - 1; y++) {
-      for (int x = 1; x < slicResult->noDims.x - 1; x++) {
+    for (int y = 1; y < slicResult->noDims.y - 1; y++)
+    {
+      for (int x = 1; x < slicResult->noDims.x - 1; x++)
+      {
         int c = slic[y * slicResult->noDims.x + x];
         if (c != slic[y * slicResult->noDims.x + x + 1] || c != slic[y * slicResult->noDims.x + x - 1] ||
             c != slic[(y - 1) * slicResult->noDims.x + x + 1] || c != slic[(y + 1) * slicResult->noDims.x + x + 1])
@@ -190,13 +205,15 @@ class Slic {
   }
 #endif
 
-  inline cv::Point2i mapToHigh(int x, int y) const {
+  inline cv::Point2i mapToHigh(int x, int y) const
+  {
     return cv::Point2i(x * spixelSize + spixelSize * 0.5, y * spixelSize + spixelSize * 0.5);
   }
 
   inline cv::Point2i mapToHigh(int index) const { return mapToHigh(index % spixelX, index / spixelY); }
 
-  inline int resampleEmptyIndex(unsigned index) const {
+  inline int resampleEmptyIndex(unsigned index) const 
+  {
     cv::Point2i coord = mapToHigh(index);
 
     if (coord.y >= slicSettings.img_size.y) coord.y = slicSettings.img_size.y - 1;
