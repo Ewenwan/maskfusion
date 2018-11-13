018a333665cfc6377b5367d2f52f29aac103259c
Wan Youwen <1756429307@qq.com>
2018-11-13 10:45:51 +0800

    * 自定义类float3、mat33的基本运算操作  * 加 减 叉积 点积 模长 归一化

--- Core/Cuda/operators.cuh
+++ Core/Cuda/operators.cuh
@@ -1,51 +1,7 @@
 /*
  * This file is part of ElasticFusion.
- *
- * Copyright (C) 2015 Imperial College London
- * 
- * The use of the code within this file and all code within files that 
- * make up the software that is ElasticFusion is permitted for 
- * non-commercial purposes only.  The full terms and conditions that 
- * apply to the code within this file are detailed within the LICENSE.txt 
- * file and at <http://www.imperial.ac.uk/dyson-robotics-lab/downloads/elastic-fusion/elastic-fusion-license/> 
- * unless explicitly stated.  By downloading this file you agree to 
- * comply with these terms.
- *
- * If you wish to use any of this code for commercial purposes then 
- * please email researchcontracts.engineering@imperial.ac.uk.
- *
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2011, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
+ * 自定义类float3、mat33的基本运算操作
+ * 加 减 叉积 点积 模长 归一化
  *  Author: Anatoly Baskeheev, Itseez Ltd, (myname.mysurname@mycompany.com)
  */
 
@@ -54,37 +10,45 @@
 #ifndef CUDA_OPERATORS_CUH_
 #define CUDA_OPERATORS_CUH_
 
+// float3 相减=====
 __device__ __host__ __forceinline__ float3 operator-(const float3& a, const float3& b)
 {
     return make_float3(a.x - b.x, a.y - b.y, a.z - b.z);
 }
-
+// float3 相加=====
 __device__ __host__ __forceinline__ float3 operator+(const float3& a, const float3& b)
 {
     return make_float3(a.x + b.x, a.y + b.y, a.z + b.z);
 }
-
+// float3 叉乘=====
+// a.x a.y a.z 
+// b.x b.y b.z
+// =======> 上中开始 a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x
 __device__ __host__ __forceinline__ float3 cross(const float3& a, const float3& b)
 {
     return make_float3(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
 }
 
+// 向量点积，对应元素乘积之和==========
 __device__ __host__ __forceinline__ float dot(const float3& a, const float3& b)
 {
     return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
+// 向量模长=========================
 __device__ __host__ __forceinline__ float norm(const float3& a)
 {
     return sqrtf(dot(a, a));
 }
 
+// 向量归一化，除以模长===============
 __device__ __host__ __forceinline__ float3 normalized(const float3& a)
 {
-    const float rn = rsqrtf(dot(a, a));
+    const float rn = rsqrtf(dot(a, a));// 快速计算模长
     return make_float3(a.x * rn, a.y * rn, a.z * rn);
 }
 
+// 3×3矩阵和 向量相乘===============
 __device__ __forceinline__ float3 operator*(const mat33& m, const float3& a)
 {
   return make_float3(dot(m.data[0], a), dot(m.data[1], a), dot(m.data[2], a));
