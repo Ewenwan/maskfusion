2e7924b72a97fd9d8e6b1361f78c139dac59ba0d
Wan Youwen <1756429307@qq.com>
2018-11-14 15:56:36 +0800

    Update Ferns.h

--- Core/Ferns.h
+++ Core/Ferns.h
@@ -1,18 +1,6 @@
 /*
  * This file is part of ElasticFusion.
- *
- * Copyright (C) 2015 Imperial College London
- *
- * The use of the code within this file and all code within files that
- * make up the software that is ElasticFusion is permitted for
- * non-commercial purposes only.  The full terms and conditions that
- * apply to the code within this file are detailed within the LICENSE.txt
- * file and at <http://www.imperial.ac.uk/dyson-robotics-lab/downloads/elastic-fusion/elastic-fusion-license/>
- * unless explicitly stated.  By downloading this file you agree to
- * comply with these terms.
- *
- * If you wish to use any of this code for commercial purposes then
- * please email researchcontracts.engineering@imperial.ac.uk.
+ * 帧=====================
  *
  */
 
@@ -29,61 +17,82 @@
 #include "Utils/RGBDOdometry.h"
 #include "Shaders/Resize.h"
 
-class Ferns {
+class Ferns 
+{
  public:
   Ferns(int n, int maxDepth, const float photoThresh);
   virtual ~Ferns();
 
-  bool addFrame(GPUTexture* imageTexture, GPUTexture* vertexTexture, GPUTexture* normalTexture, const Eigen::Matrix4f& pose, int srcTime,
+  bool addFrame(GPUTexture* imageTexture, 
+                GPUTexture* vertexTexture, 
+                GPUTexture* normalTexture, 
+                const Eigen::Matrix4f& pose, 
+                int srcTime,
                 const float threshold);
 
-  class SurfaceConstraint {
+  class SurfaceConstraint 
+  {
    public:
-    SurfaceConstraint(const Eigen::Vector4f& sourcePoint, const Eigen::Vector4f& targetPoint)
+    SurfaceConstraint(const Eigen::Vector4f& sourcePoint, 
+                      const Eigen::Vector4f& targetPoint)
         : sourcePoint(sourcePoint), targetPoint(targetPoint) {}
 
     Eigen::Vector4f sourcePoint;
     Eigen::Vector4f targetPoint;
   };
 
-  Eigen::Matrix4f findFrame(std::vector<SurfaceConstraint>& constraints, const Eigen::Matrix4f& currPose, GPUTexture* vertexTexture,
-                            GPUTexture* normalTexture, GPUTexture* imageTexture, const int time, const bool lost);
+  Eigen::Matrix4f findFrame(std::vector<SurfaceConstraint>& constraints, 
+                            const Eigen::Matrix4f& currPose, 
+                            GPUTexture* vertexTexture,
+                            GPUTexture* normalTexture,
+                            GPUTexture* imageTexture, 
+                            const int time, 
+                            const bool lost);
 
-  class Fern {
+  class Fern 
+  {
    public:
     Fern() {}
 
-    Eigen::Vector2i pos;
-    Eigen::Vector4i rgbd;
+    Eigen::Vector2i pos; // 像素坐标
+    Eigen::Vector4i rgbd;// 色彩+深度
     std::vector<int> ids[16];
   };
 
   std::vector<Fern> conservatory;
 
-  class Frame {
+  class Frame 
+  {
    public:
-    Frame(int n, int id, const Eigen::Matrix4f& pose, const int srcTime, const int numPixels, unsigned char* rgb = 0,
+    Frame(int n, int id, const Eigen::Matrix4f& pose, 
+          const int srcTime, const int numPixels, unsigned char* rgb = 0,
           Eigen::Vector4f* verts = 0, Eigen::Vector4f* norms = 0)
-        : goodCodes(0), id(id), pose(pose), srcTime(srcTime), initRgb(rgb), initVerts(verts), initNorms(norms) {
+        : goodCodes(0), id(id), pose(pose), srcTime(srcTime), 
+          initRgb(rgb), initVerts(verts), initNorms(norms)
+    {
       codes = new unsigned char[n];
 
-      if (rgb) {
+      if (rgb) 
+      {
         this->initRgb = new unsigned char[numPixels * 3];
         memcpy(this->initRgb, rgb, numPixels * 3);
       }
 
-      if (verts) {
+      if (verts) 
+      {
         this->initVerts = new Eigen::Vector4f[numPixels];
         memcpy(this->initVerts, verts, numPixels * sizeof(Eigen::Vector4f));
       }
 
-      if (norms) {
+      if (norms) 
+      {
         this->initNorms = new Eigen::Vector4f[numPixels];
         memcpy(this->initNorms, norms, numPixels * sizeof(Eigen::Vector4f));
       }
     }
 
-    virtual ~Frame() {
+    virtual ~Frame() 
+    {
       delete[] codes;
 
       if (initRgb) delete[] initRgb;
@@ -127,8 +136,11 @@ class Ferns {
   float blockHD(const Frame* f1, const Frame* f2);
   float blockHDAware(const Frame* f1, const Frame* f2);
 
-  float photometricCheck(const Img<Eigen::Vector4f>& vertSmall, const Img<Eigen::Matrix<unsigned char, 3, 1>>& imgSmall,
-                         const Eigen::Matrix4f& estPose, const Eigen::Matrix4f& fernPose, const unsigned char* fernRgb);
+  float photometricCheck(const Img<Eigen::Vector4f>& vertSmall, 
+                         const Img<Eigen::Matrix<unsigned char, 3, 1>>& imgSmall,
+                         const Eigen::Matrix4f& estPose, 
+                         const Eigen::Matrix4f& fernPose,
+                         const unsigned char* fernRgb);
 
   GPUTexture vertFern;
   GPUTexture vertCurrent;
