1e33b4fff3584337b334e4a597a22f18f5b11347
Wan Youwen <1756429307@qq.com>
2018-11-14 14:35:28 +0800

    Update ConnectedLabels.hpp

--- Core/Segmentation/ConnectedLabels.hpp
+++ Core/Segmentation/ConnectedLabels.hpp
@@ -1,18 +1,6 @@
 /*
  * This file is part of https://github.com/martinruenz/maskfusion
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
  */
 
 #pragma once
@@ -20,12 +8,16 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <map>
 
-struct ComponentData {
-  unsigned char label;
+// 组建数据结构=============================
+struct ComponentData 
+{
+  unsigned char label; //类别标签
+  // 边框
   int top = std::numeric_limits<int>::max();
   int right = 0;
   int bottom = 0;
   int left = std::numeric_limits<int>::max();
+  // 中心点
   float centerX = 0;
   float centerY = 0;
   int size = 0;
@@ -37,9 +29,11 @@ struct ComponentData {
 //    for(unsigned i=0; i < ccStats.size(); i++) labelToComponents[ccStats[i].label].push_back(i);
 //}
 
-std::map<int, std::list<int>> mapLabelsToComponents(const std::vector<ComponentData>& ccStats) {
-  std::map<int, std::list<int>> labelToComponents;
-  for (unsigned i = 0; i < ccStats.size(); i++) {
+std::map<int, std::list<int>> mapLabelsToComponents(const std::vector<ComponentData>& ccStats)
+{
+  std::map<int, std::list<int>> labelToComponents;// 1 对 多
+  for (unsigned i = 0; i < ccStats.size(); i++) 
+  {
     auto r = labelToComponents.insert({ccStats[i].label, std::list<int>()});
     r.first->second.push_back(i);
   }
@@ -47,31 +41,39 @@ std::map<int, std::list<int>> mapLabelsToComponents(const std::vector<ComponentD
   return labelToComponents;
 }
 
-cv::Mat connectedLabels(cv::Mat input, std::vector<ComponentData>* stats) {
+cv::Mat connectedLabels(cv::Mat input, std::vector<ComponentData>* stats) 
+{
   assert(input.type() == CV_8UC1);
 
   cv::Mat componentsImg(input.rows, input.cols, cv::DataType<int>::type);
   int* componentRowPtr = componentsImg.ptr<int>(0);
 
   std::vector<int> componentRoots;
-  auto newComponent = [&componentRoots]() -> int {
+  auto newComponent = [&componentRoots]() -> int 
+  {
     int r = componentRoots.size();
     componentRoots.push_back(r);
     return r;
   };
-  auto findRoot = [&componentRoots](int index) -> int {
+  
+  auto findRoot = [&componentRoots](int index) -> int 
+  {
     while (true) {
       if (index == componentRoots[index]) return index;
       index = componentRoots[index];
     }
   };
-  auto merge = [&](int index1, int index2) -> int {
+  
+  auto merge = [&](int index1, int index2) -> int
+  {
     int r1 = findRoot(index1);
     int r2 = findRoot(index2);
     if (r1 < r2) {
       componentRoots[r2] = r1;
       return r1;
-    } else {
+    } 
+    else
+    {
       componentRoots[r1] = r2;
       return r2;
     }
@@ -81,7 +83,8 @@ cv::Mat connectedLabels(cv::Mat input, std::vector<ComponentData>* stats) {
 
   // First row
   componentRowPtr[0] = newComponent();
-  for (int c = 1; c < input.cols; c++) {
+  for (int c = 1; c < input.cols; c++) 
+  {
     if (input.data[c] == input.data[c - 1])
       componentRowPtr[c] = componentRowPtr[c - 1];
     else
@@ -91,7 +94,8 @@ cv::Mat connectedLabels(cv::Mat input, std::vector<ComponentData>* stats) {
   // Other rows
   uchar* lastRowPtr = input.ptr<uchar>(0);
   int* lastComponentRowPtr = componentRowPtr;
-  for (int r = 1; r < input.rows; r++) {
+  for (int r = 1; r < input.rows; r++) 
+  {
     uchar* rowPtr = input.ptr<uchar>(r);
     componentRowPtr = componentsImg.ptr<int>(r);
     // First column
@@ -100,20 +104,29 @@ cv::Mat connectedLabels(cv::Mat input, std::vector<ComponentData>* stats) {
     else
       componentRowPtr[0] = newComponent();
     // Other columns
-    for (int c = 1; c < input.cols; c++) {
+    for (int c = 1; c < input.cols; c++)
+    {
       // const uchar val = rowPtr[c];
-      if (rowPtr[c] == rowPtr[c - 1]) {
+      if (rowPtr[c] == rowPtr[c - 1]) 
+      {
         int cLeft = componentRowPtr[c - 1];
         int cTop = lastComponentRowPtr[c];
-        if (rowPtr[c] == lastRowPtr[c] && cLeft != cTop) {
+        if (rowPtr[c] == lastRowPtr[c] && cLeft != cTop) 
+        {
           // Found merging situation
           componentRowPtr[c] = merge(cTop, cLeft);
-        } else {
+        } 
+        else
+        {
           componentRowPtr[c] = cLeft;
         }
-      } else if (rowPtr[c] == lastRowPtr[c]) {
+      } 
+      else if (rowPtr[c] == lastRowPtr[c]) 
+      {
         componentRowPtr[c] = lastComponentRowPtr[c];
-      } else {
+      } 
+      else 
+      {
         componentRowPtr[c] = newComponent();
       }
     }
@@ -126,24 +139,31 @@ cv::Mat connectedLabels(cv::Mat input, std::vector<ComponentData>* stats) {
   // std::map<int,int> rootMapping; // A map would save memory but is probably slower
   std::vector<int> rootMapping(componentRoots.size());
   int rootCnt = 0;
-  for (unsigned id = 0; id < componentRoots.size(); id++) {
+  for (unsigned id = 0; id < componentRoots.size(); id++)
+  {
     int root = findRoot(id);
-    if ((unsigned)root == id) {
+    if ((unsigned)root == id) 
+    {
       rootMapping[root] = rootCnt++;
-    } else {
+    } 
+    else
+    {
       componentRoots[id] = root;
     }
   }
   for (auto& c : componentRoots) c = rootMapping[c];
 
   // Apply second pass and optionally compute some stats
-  if (stats) {
+  if (stats)
+  {
     assert(stats->size() == 0);
     stats->resize(rootCnt);
-    for (int y = 0; y < componentsImg.rows; y++) {
+    for (int y = 0; y < componentsImg.rows; y++)
+    {
       componentRowPtr = componentsImg.ptr<int>(y);
       uchar* rowPtr = input.ptr<uchar>(y);
-      for (int x = 0; x < componentsImg.cols; x++) {
+      for (int x = 0; x < componentsImg.cols; x++) 
+      {
         int c = componentRoots[componentRowPtr[x]];
         componentRowPtr[x] = c;
         ComponentData& data = (*stats)[c];
@@ -157,7 +177,8 @@ cv::Mat connectedLabels(cv::Mat input, std::vector<ComponentData>* stats) {
         if (x > data.right) data.right = x;
       }
     }
-    for (ComponentData& data : *stats) {
+    for (ComponentData& data : *stats) 
+    {
       data.centerX /= data.size;
       data.centerY /= data.size;
     }
