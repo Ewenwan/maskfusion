fd4575a8999d03616c5923ae6e8146b857c6de0d
Wan Youwen <1756429307@qq.com>
2018-11-14 16:00:54 +0800

    Update Ferns.cpp

--- Core/Ferns.cpp
+++ Core/Ferns.cpp
@@ -1,18 +1,6 @@
 /*
  * This file is part of ElasticFusion.
- *
- * Copyright (C) 2015 Imperial College London
- *
- * The use of the code within this file and all code within files that
- * make up the software that is ElasticFusion is permitted for
- * non-commercial purposes only.  The full terms and conditions that
- * apply to the code within this file are detailed within the LICENSE.txt
- * file and at <http://www.imperial.ac.uk/dyson-robotics-lab/downloads/elastic-fusion/elastic-fusion-license/>
- * unless explicitly stated.  By downloading this file you agree to
- * comply with these terms.
- *
- * If you wish to use any of this code for commercial purposes then
- * please email researchcontracts.engineering@imperial.ac.uk.
+ * 
  *
  */
 
@@ -31,30 +19,40 @@ Ferns::Ferns(int n, int maxDepth, const float photoThresh)
       dDist(400, maxDepth),
       lastClosest(-1),
       badCode(255),
-      rgbd(Resolution::getInstance().width() / factor, Resolution::getInstance().height() / factor, Intrinsics::getInstance().cx() / factor,
-           Intrinsics::getInstance().cy() / factor, Intrinsics::getInstance().fx() / factor, Intrinsics::getInstance().fy() / factor),
+      rgbd(Resolution::getInstance().width() / factor, 
+           Resolution::getInstance().height() / factor, 
+           Intrinsics::getInstance().cx() / factor,
+           Intrinsics::getInstance().cy() / factor, 
+           Intrinsics::getInstance().fx() / factor, 
+           Intrinsics::getInstance().fy() / factor),
       vertFern(width, height, GL_RGBA32F, GL_LUMINANCE, GL_FLOAT, false, true),
       vertCurrent(width, height, GL_RGBA32F, GL_LUMINANCE, GL_FLOAT, false, true),
       normFern(width, height, GL_RGBA32F, GL_LUMINANCE, GL_FLOAT, false, true),
       normCurrent(width, height, GL_RGBA32F, GL_LUMINANCE, GL_FLOAT, false, true),
       colorFern(width, height, GL_RGBA, GL_RGB, GL_UNSIGNED_BYTE, false, true),
       colorCurrent(width, height, GL_RGBA, GL_RGB, GL_UNSIGNED_BYTE, false, true),
-      resize(Resolution::getInstance().width(), Resolution::getInstance().height(), width, height),
+      resize(Resolution::getInstance().width(), 
+             Resolution::getInstance().height(), width, height),
       imageBuff(width, height),
       vertBuff(width, height),
-      normBuff(width, height) {
+      normBuff(width, height) 
+{
   random.seed(time(0));
   generateFerns();
 }
 
-Ferns::~Ferns() {
-  for (size_t i = 0; i < frames.size(); i++) {
+Ferns::~Ferns() 
+{
+  for (size_t i = 0; i < frames.size(); i++)
+  {
     delete frames.at(i);
   }
 }
 
-void Ferns::generateFerns() {
-  for (int i = 0; i < num; i++) {
+void Ferns::generateFerns() 
+{
+  for (int i = 0; i < num; i++) 
+  {
     Fern f;
 
     f.pos(0) = widthDist(random);
@@ -69,8 +67,12 @@ void Ferns::generateFerns() {
   }
 }
 
-bool Ferns::addFrame(GPUTexture* imageTexture, GPUTexture* vertexTexture, GPUTexture* normalTexture, const Eigen::Matrix4f& pose,
-                     int srcTime, const float threshold) {
+bool Ferns::addFrame(GPUTexture* imageTexture, 
+                     GPUTexture* vertexTexture, 
+                     GPUTexture* normalTexture, 
+                     const Eigen::Matrix4f& pose,
+                     int srcTime, const float threshold) 
+{
   Img<Eigen::Matrix<unsigned char, 3, 1>> img(height, width);
   Img<Eigen::Vector4f> verts(height, width);
   Img<Eigen::Vector4f> norms(height, width);
@@ -79,19 +81,26 @@ bool Ferns::addFrame(GPUTexture* imageTexture, GPUTexture* vertexTexture, GPUTex
   resize.vertex(vertexTexture, verts);
   resize.vertex(normalTexture, norms);
 
-  Frame* frame = new Frame(num, frames.size(), pose, srcTime, width * height, (unsigned char*)img.data, (Eigen::Vector4f*)verts.data,
+  Frame* frame = new Frame(num, frames.size(), pose, 
+                           srcTime, width * height, 
+                           (unsigned char*)img.data,
+                           (Eigen::Vector4f*)verts.data,
                            (Eigen::Vector4f*)norms.data);
 
   int* coOccurrences = new int[frames.size()];
 
   memset(coOccurrences, 0, sizeof(int) * frames.size());
 
-  for (int i = 0; i < num; i++) {
+  for (int i = 0; i < num; i++)
+  {
     unsigned char code = badCode;
 
-    if (verts.at<Eigen::Vector4f>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(2) > 0) {
+    if (verts.at<Eigen::Vector4f>(conservatory.at(i).pos(1),
+                                  conservatory.at(i).pos(0))(2) > 0) 
+    {
       const Eigen::Matrix<unsigned char, 3, 1>& pix =
-          img.at<Eigen::Matrix<unsigned char, 3, 1>>(conservatory.at(i).pos(1), conservatory.at(i).pos(0));
+          img.at<Eigen::Matrix<unsigned char, 3, 1>>(conservatory.at(i).pos(1), 
+                                                     conservatory.at(i).pos(0));
 
       code =
           (pix(0) > conservatory.at(i).rgbd(0)) << 3 | (pix(1) > conservatory.at(i).rgbd(1)) << 2 |
@@ -100,7 +109,8 @@ bool Ferns::addFrame(GPUTexture* imageTexture, GPUTexture* vertexTexture, GPUTex
 
       frame->goodCodes++;
 
-      for (size_t j = 0; j < conservatory.at(i).ids[code].size(); j++) {
+      for (size_t j = 0; j < conservatory.at(i).ids[code].size(); j++)
+      {
         coOccurrences[conservatory.at(i).ids[code].at(j)]++;
       }
     }
@@ -110,13 +120,16 @@ bool Ferns::addFrame(GPUTexture* imageTexture, GPUTexture* vertexTexture, GPUTex
 
   float minimum = std::numeric_limits<float>::max();
 
-  if (frame->goodCodes > 0) {
-    for (size_t i = 0; i < frames.size(); i++) {
+  if (frame->goodCodes > 0) 
+  {
+    for (size_t i = 0; i < frames.size(); i++) 
+    {
       float maxCo = std::min(frame->goodCodes, frames.at(i)->goodCodes);
 
       float dissim = (float)(maxCo - coOccurrences[i]) / (float)maxCo;
 
-      if (dissim < minimum) {
+      if (dissim < minimum) 
+      {
         minimum = dissim;
       }
     }
@@ -124,9 +137,12 @@ bool Ferns::addFrame(GPUTexture* imageTexture, GPUTexture* vertexTexture, GPUTex
 
   delete[] coOccurrences;
 
-  if ((minimum > threshold || frames.size() == 0) && frame->goodCodes > 0) {
-    for (int i = 0; i < num; i++) {
-      if (frame->codes[i] != badCode) {
+  if ((minimum > threshold || frames.size() == 0) && frame->goodCodes > 0)
+  {
+    for (int i = 0; i < num; i++) 
+    {
+      if (frame->codes[i] != badCode) 
+      {
         conservatory.at(i).ids[frame->codes[i]].push_back(frame->id);
       }
     }
@@ -134,15 +150,22 @@ bool Ferns::addFrame(GPUTexture* imageTexture, GPUTexture* vertexTexture, GPUTex
     frames.push_back(frame);
 
     return true;
-  } else {
+  } 
+  else 
+  {
     delete frame;
 
     return false;
   }
 }
 
-Eigen::Matrix4f Ferns::findFrame(std::vector<SurfaceConstraint>& constraints, const Eigen::Matrix4f& currPose, GPUTexture* vertexTexture,
-                                 GPUTexture* normalTexture, GPUTexture* imageTexture, const int time, const bool lost) {
+Eigen::Matrix4f Ferns::findFrame(std::vector<SurfaceConstraint>& constraints,
+                                 const Eigen::Matrix4f& currPose, 
+                                 GPUTexture* vertexTexture,
+                                 GPUTexture* normalTexture, 
+                                 GPUTexture* imageTexture,
+                                 const int time, const bool lost) 
+{
   lastClosest = -1;
 
   Img<Eigen::Matrix<unsigned char, 3, 1>> imgSmall(height, width);
@@ -162,9 +185,12 @@ Eigen::Matrix4f Ferns::findFrame(std::vector<SurfaceConstraint>& constraints, co
   for (int i = 0; i < num; i++) {
     unsigned char code = badCode;
 
-    if (vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(2) > 0) {
+    if (vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), 
+                                      conservatory.at(i).pos(0))(2) > 0) 
+    {
       const Eigen::Matrix<unsigned char, 3, 1>& pix =
-          imgSmall.at<Eigen::Matrix<unsigned char, 3, 1>>(conservatory.at(i).pos(1), conservatory.at(i).pos(0));
+          imgSmall.at<Eigen::Matrix<unsigned char, 3, 1>>(conservatory.at(i).pos(1), 
+                                                          conservatory.at(i).pos(0));
 
       code = (pix(0) > conservatory.at(i).rgbd(0)) << 3 | (pix(1) > conservatory.at(i).rgbd(1)) << 2 |
              (pix(2) > conservatory.at(i).rgbd(2)) << 1 |
@@ -173,7 +199,8 @@ Eigen::Matrix4f Ferns::findFrame(std::vector<SurfaceConstraint>& constraints, co
 
       frame->goodCodes++;
 
-      for (size_t j = 0; j < conservatory.at(i).ids[code].size(); j++) {
+      for (size_t j = 0; j < conservatory.at(i).ids[code].size(); j++) 
+      {
         coOccurrences[conservatory.at(i).ids[code].at(j)]++;
       }
     }
@@ -184,12 +211,14 @@ Eigen::Matrix4f Ferns::findFrame(std::vector<SurfaceConstraint>& constraints, co
   float minimum = std::numeric_limits<float>::max();
   int minId = -1;
 
-  for (size_t i = 0; i < frames.size(); i++) {
+  for (size_t i = 0; i < frames.size(); i++) 
+  {
     float maxCo = std::min(frame->goodCodes, frames.at(i)->goodCodes);
 
     float dissim = (float)(maxCo - coOccurrences[i]) / (float)maxCo;
 
-    if (dissim < minimum && time - frames.at(i)->srcTime > 300) {
+    if (dissim < minimum && time - frames.at(i)->srcTime > 300)
+    {
       minimum = dissim;
       minId = i;
     }
@@ -199,7 +228,8 @@ Eigen::Matrix4f Ferns::findFrame(std::vector<SurfaceConstraint>& constraints, co
 
   Eigen::Matrix4f estPose = Eigen::Matrix4f::Identity();
 
-  if (minId != -1 && blockHDAware(frame, frames.at(minId)) > 0.3) {
+  if (minId != -1 && blockHDAware(frame, frames.at(minId)) > 0.3) 
+  {
     Eigen::Matrix4f fernPose = frames.at(minId)->pose;
 
     vertFern.texture->Upload(frames.at(minId)->initVerts, GL_RGBA, GL_FLOAT);
@@ -231,10 +261,12 @@ Eigen::Matrix4f Ferns::findFrame(std::vector<SurfaceConstraint>& constraints, co
 
     //        std::cout << rgbd.lastICPError << ", " << rgbd.lastICPCount << ", " << photoError << std::endl;
 
-    if (rgbd.lastICPError < 0.0003 && rgbd.lastICPCount > icpCountThresh && photoError < photoThresh) {
+    if (rgbd.lastICPError < 0.0003 && rgbd.lastICPCount > icpCountThresh && photoError < photoThresh) 
+    {
       lastClosest = minId;
 
-      for (int i = 0; i < num; i += num / 50) {
+      for (int i = 0; i < num; i += num / 50)
+      {
         if (vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(2) > 0 &&
             int(vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(2) * 1000.0f) < maxDepth) {
           Eigen::Vector4f worldRawPoint =
@@ -258,8 +290,12 @@ Eigen::Matrix4f Ferns::findFrame(std::vector<SurfaceConstraint>& constraints, co
   return estPose;
 }
 
-float Ferns::photometricCheck(const Img<Eigen::Vector4f>& vertSmall, const Img<Eigen::Matrix<unsigned char, 3, 1>>& imgSmall,
-                              const Eigen::Matrix4f& estPose, const Eigen::Matrix4f& fernPose, const unsigned char* fernRgb) {
+float Ferns::photometricCheck(const Img<Eigen::Vector4f>& vertSmall, 
+                              const Img<Eigen::Matrix<unsigned char, 3, 1>>& imgSmall,
+                              const Eigen::Matrix4f& estPose,
+                              const Eigen::Matrix4f& fernPose, 
+                              const unsigned char* fernRgb) 
+{
   float cx = Intrinsics::getInstance().cx() / factor;
   float cy = Intrinsics::getInstance().cy() / factor;
   float invfx = 1.0f / float(Intrinsics::getInstance().fx() / factor);
@@ -270,9 +306,12 @@ float Ferns::photometricCheck(const Img<Eigen::Vector4f>& vertSmall, const Img<E
   float photoSum = 0;
   int photoCount = 0;
 
-  for (int i = 0; i < num; i++) {
+  for (int i = 0; i < num; i++) 
+  {
     if (vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(2) > 0 &&
-        int(vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(2) * 1000.0f) < maxDepth) {
+        int(vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), 
+                                          conservatory.at(i).pos(0))(2) * 1000.0f) < maxDepth)
+    {
       Eigen::Vector4f vertPoint =
           Eigen::Vector4f(vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(0),
                           vertSmall.at<Eigen::Vector4f>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(1),
@@ -288,7 +327,8 @@ float Ferns::photometricCheck(const Img<Eigen::Vector4f>& vertSmall, const Img<E
       if (correspondence(0) >= 0 && correspondence(1) >= 0 && correspondence(0) < width && correspondence(1) < height &&
           (imgFern.at<Eigen::Matrix<unsigned char, 3, 1>>(correspondence(1), correspondence(0))(0) > 0 ||
            imgFern.at<Eigen::Matrix<unsigned char, 3, 1>>(correspondence(1), correspondence(0))(1) > 0 ||
-           imgFern.at<Eigen::Matrix<unsigned char, 3, 1>>(correspondence(1), correspondence(0))(2) > 0)) {
+           imgFern.at<Eigen::Matrix<unsigned char, 3, 1>>(correspondence(1), correspondence(0))(2) > 0)) 
+      {
         photoSum += abs((int)imgFern.at<Eigen::Matrix<unsigned char, 3, 1>>(correspondence(1), correspondence(0))(0) -
                         (int)imgSmall.at<Eigen::Matrix<unsigned char, 3, 1>>(conservatory.at(i).pos(1), conservatory.at(i).pos(0))(0));
         photoSum += abs((int)imgFern.at<Eigen::Matrix<unsigned char, 3, 1>>(correspondence(1), correspondence(0))(1) -
@@ -303,7 +343,8 @@ float Ferns::photometricCheck(const Img<Eigen::Vector4f>& vertSmall, const Img<E
   return photoSum / float(photoCount);
 }
 
-float Ferns::blockHD(const Frame* f1, const Frame* f2) {
+float Ferns::blockHD(const Frame* f1, const Frame* f2) 
+{
   float sum = 0.0f;
 
   for (int i = 0; i < num; i++) {
@@ -315,15 +356,18 @@ float Ferns::blockHD(const Frame* f1, const Frame* f2) {
   return sum;
 }
 
-float Ferns::blockHDAware(const Frame* f1, const Frame* f2) {
+float Ferns::blockHDAware(const Frame* f1, const Frame* f2) 
+{
   int count = 0;
   float val = 0;
 
   for (int i = 0; i < num; i++) {
-    if (f1->codes[i] != badCode && f2->codes[i] != badCode) {
+    if (f1->codes[i] != badCode && f2->codes[i] != badCode) 
+    {
       count++;
 
-      if (f1->codes[i] == f2->codes[i]) {
+      if (f1->codes[i] == f2->codes[i]) 
+      {
         val += 1.0f;
       }
     }
