ffcdb7998ceaacd9f9c10faec8ee248fbf435ba1
Wan Youwen <1756429307@qq.com>
2018-11-14 16:09:23 +0800

    帧 数据结构 语义分割mask  色彩RGB 深度值 目标类别id 序列 目标 框 序列

--- Core/FrameData.h
+++ Core/FrameData.h
@@ -1,6 +1,7 @@
 /*
  * This file is part of https://github.com/martinruenz/maskfusion
  * 帧 数据结构====================================================
+ 语义分割mask  色彩RGB 深度值 目标类别id 序列 目标 框 序列
  */
 
 #pragma once
